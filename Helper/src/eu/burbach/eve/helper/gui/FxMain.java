5f95121f3585398f519778723c634b53c6dfc408
Holger Burbach <holger@burbach.eu>
2017-04-23 15:15:29 +0200

    Datum korrigiert

--- Helper/src/eu/burbach/eve/helper/gui/FxMain.java
+++ Helper/src/eu/burbach/eve/helper/gui/FxMain.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class FxMain extends Application {
 	
-	public static final String VERSION="22.04.2017";
+	public static final String VERSION="23.04.2017";
 
 	public static Stage st=null;
 	
